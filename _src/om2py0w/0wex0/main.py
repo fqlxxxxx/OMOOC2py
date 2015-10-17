8e46c919a129d7a46de3d3e6fdf73c01a4cc0c71
alex <alex.fql@gmail.com>
2015-10-17 23:59:36 +0800

    w3

---
+++ _src/om2py0w/0wex0/main.py
@@ -0,0 +1,42 @@
+-*- coding: utf-8 -*-
+
+import os
+
+def main():
+	print "Hello world!"
+
+	print "这是 Alice\'的问候."
+	print '这是 Bob \'的问候.'
+
+	foo(5, 10)
+
+	print '=' * 10
+	print '这将直接执行' + os.getcwd()
+
+	counter = 0 
+	counter += 1
+
+	food = ['苹果', '杏子', '李子', '梨']
+	for i in food:
+		print '俺就爱整只:' +i 
+	
+	print '数到10'
+		print
+
+def foo(paraml, secondparam)
+	res = paraml + secondp
+	print '&s 加 &s' 等于 &s '(paraaml, secondparam, res)
+	withall
+	print '数道'
+	print: 术导
+	elif
+
+
+
+
+
+
+elif(res < 50)
+
+
+elif()
