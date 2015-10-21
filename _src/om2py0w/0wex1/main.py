eba090340faaf6eec24da8d0fa6e6bedc4f0ffe6
alex <alex.fql@gmail.com>
2015-10-21 18:23:19 +0800

    V1.1
    有 bug
    
    1,查询时输入非存在文件报错
    2,如果不想查询退出时报错

--- _src/om2py0w/0wex1/main.py
+++ _src/om2py0w/0wex1/main.py
@@ -1,6 +1,9 @@
 #-*- coding: UTF-8 -*-
 
+
+
 from sys import argv
+from os.path import exists
 
 script, text = argv
 
@@ -16,4 +19,24 @@ txt.write(content)
 txt.close
 
 print "已经保存日记"
+
+
+print "是否要继续查看其他日记? 点击回车继续! 退出 control+C"
+
+raw_input(">")
+
+txt = raw_input("请输入日记名称:")
+
+print "日记是否存在? %r" % exists(txt)
+
+R = open(txt)
+
+print R.read()
+
+R.close
+
+
+
+
+
 
