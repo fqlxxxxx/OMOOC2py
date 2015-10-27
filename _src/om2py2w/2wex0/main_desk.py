f3e2517814f7e493cbc4e917068c7496fe87a85e
alex <alex.fql@gmail.com>
2015-10-27 11:45:14 +0800

    修改窗体头部名称

--- _src/om2py2w/2wex0/main_desk.py
+++ _src/om2py2w/2wex0/main_desk.py
@@ -4,4 +4,7 @@
 from Tkinter import *
 root = Tk()
 Frame(height=700, width=600, bg="white").pack()
+# 修改 frame 窗体头部名称
+root.title(" 极简日记本")
+Label(root, height=20, width=70, text="2015年9月10日", fg="lightblue").pack()
 root.mainloop()
