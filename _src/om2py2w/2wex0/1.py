b2ef323eb700e0043b6c90f1d1bc101878e5e5b1
alex <alex.fql@gmail.com>
2015-10-29 18:14:53 +0800

    实现输入,显示

--- _src/om2py2w/2wex0/1.py
+++ _src/om2py2w/2wex0/1.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 
 
-from Tkinter import *  # import Tkinter module
 
-#
+'''
 
 
 class Application(Frame):
@@ -35,3 +34,34 @@ app = Application()
 app.master.title("极简日记本")
 
 app.mainloop()
+
+'''
+
+from Tkinter import *
+
+def app_text():
+        text = e2.get()
+        e1.insert(END, text)
+
+def ins_text():
+        text = e2.get()
+        e1.insert(0, text)
+
+def clr_text():
+        e1.delete(0, END)
+
+root = Tk()
+
+e1 = Entry()
+e2 = Entry()
+b1 = Button(text = "Append Text", command = app_text)
+b2 = Button(text = "Insert Text", command = ins_text)
+b3 = Button(text = "Clear Text", command = clr_text)
+
+e1.pack(padx = 10, pady = 10, fill = X)
+e2.pack(padx = 10, pady = 10, fill = X)
+b1.pack(padx = 10, pady = 5)
+b2.pack(padx = 10, pady = 5)
+b3.pack(padx = 10, pady = 5)
+
+root.mainloop()
--- _src/om2py2w/2wex0/main_desk.py
+++ _src/om2py2w/2wex0/main_desk.py
@@ -3,9 +3,29 @@
 
 from Tkinter import *  # import Tkinter module
 
-#
+
+def save_text():
+    t = entry.get()
+    text.insert(END, t)
+
+root = Tk()
+root.title("极简日记本")
+
+text = Text(root, height=20, width=40)
+entry = Entry(root, bg="gray")
+
+buttonqiut = Button(root, text="退出", command=quit)
+buttonadd = Button(root, text="保存", command=save_text)
+
+text.pack()
+entry.pack(side=LEFT)
+buttonadd.pack(side=RIGHT)
+buttonqiut.pack(side=RIGHT)
+
+root.mainloop()
 
 
+'''
 class Application(Frame):
 
     def __init__(self, master=None):
@@ -15,23 +35,31 @@ class Application(Frame):
 
     def mydarily(self):
         # 添加显示区域
-        self.text = Text(self, bg="gray", height=20, width=40)
+        self.text = Text(self, height=20, width=40, state=DISABLED)
         self.text.pack()
+
+        # 添加输入框
+        self.entry = Entry(self, bg="blue")
+        self.entry.pack(side=LEFT)
+
+    def text(self):
+        t = self.entry.get()
+        self.text.insert(0, t)      
+
         # 添加退出按钮
+    def mydarily(self):
         self.buttonquit = Button(self, text="退出", command=self.quit)
         self.buttonquit.pack(side=RIGHT)
+
         # 添加保存按钮
-        self.buttonadd = Button(self, text="保存")
+    def mydarily(self):
+        self.buttonadd = Button(self, text="保存", command=self.text)
         # 绑定按钮
         self.buttonadd.bind()
         self.buttonadd.pack(side=RIGHT)
-        # 添加输入框
-        self.entry = Entry(self, bg="blue")
-        g = self.entry.get()
-        self.entry.pack(side=LEFT)
-
 
 app = Application()
 app.master.title("极简日记本")
 
 app.mainloop()
+'''
