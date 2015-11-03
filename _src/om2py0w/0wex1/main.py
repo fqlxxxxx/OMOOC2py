45c82801d76a3aa0e8c798d69e9ead69b6d6d4eb
alex <alex.fql@gmail.com>
2015-11-03 18:45:56 +0800

    时间修正，秒6位变2位，用raw_input替代argv

--- _src/om2py0w/0wex1/main.py
+++ _src/om2py0w/0wex1/main.py
@@ -1,20 +1,19 @@
 #-*- coding: UTF-8 -*-
 
 
-from sys import argv
 from os.path import exists
 import datetime  # 导入时间模块
 
-script, text = argv
 
-if exists(text):
+while exists("log.txt"):
     print "历史日记:"
-    p = open(text, "r")
+    p = open("log.txt", "r")
     print p.read()
+    break
 
 while True:
     print "添加新日记:"
-    now = datetime.datetime.now()  # 获取当前时间
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # 获取当前时间
     input1 = raw_input(">")
     if input1 == "quit":  # 调整代码位置解决 quit 写入 txt 中
         input2 = raw_input("真要退出?y/n:")
@@ -24,8 +23,8 @@ while True:
         if input2 == "n":
             continue
 
-    content = open(text, "a")
-    content.write(input1)
-    content.write("%s" % now)  # 记录时间, write 只能写入字符串, 日期无法直接写入, 因此用字符串替换的方式转换一下
-    content.write("\n")  # 解决写入日记换行问题
+    content = open("log.txt", "a")
+    content.write(input1 + now + "\n")
+    # content.write("%s" % now)  # 另外一种将时间变为文本格式的方法, 记录时间, write 只能写入字符串, 日期无法直接写入, 因此用字符串替换的方式转换一下
+    # content.write("\n")  # 解决写入日记换行问题
     content.close
