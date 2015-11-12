166cbbb686a548016df3ce6f82a49820280692ad
alex <alex.fql@gmail.com>
2015-11-12 18:43:21 +0800

    1

--- _src/om2py4w/4wex0/diary_web.py
+++ _src/om2py4w/4wex0/diary_web.py
@@ -1,28 +1,36 @@
-#-*- coding: UTF-8 -*-
-from bottle import route, run, jinja2_template
-#from jinja2 import Environment, PackageLoader
-# env = Environment(loader=PackageLoader('/templates'))
+# -*- coding: UTF-8 -*-
+
+
+from bottle import route, run, jinja2_template, request, get, post
+
 
 # 主页面
 @route("/")
 @route("/index")
 def index():
-    # template = env.get_template('index.html')
-    return jinja2_template('base.html')
+    return jinja2_template('index.html')
+
+
+# 写日记页面
+@route("/write_diary")
+@get("/write_diary")
+def write_diary_form():
+    return jinja2_template('write_diary.html')
+
+
+@route("/write_diary")
+@post("/write_diary")
+def write_diary():
+    diary_content = request.forms.get('diary_content')
+    return jinja2_template('write_diary.html', diary_content=diary_content)
+
+
+
+# 查看历史笔记
+@route("/history")
+def history():
+    return jinja2_template('history.html')
+
 
 run(host='localhost', port=5000, reloader=True)
 
-
-# 保存笔记
-
-
-# 查看历史笔记页面
-
-
-
-
-
-                 #@route('/hello/<name>')
-                 # def hello(name='stranger'):
-                 #    return template('Hello {{name}}, how are you?', name=name)
-                 # run(host='localhost', port=8080)
